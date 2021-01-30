869867b7de6f287206a4009b3723bdf24e933420
harshita <harshita.sharma1729@gmail.com>
2021-01-31 01:40:47 +0530

    fetch link as struct
    Signed-off-by: harshita <harshita.sharma1729@gmail.com>

--- go-exercises/parse/parse.go
+++ go-exercises/parse/parse.go
@@ -8,10 +8,10 @@ import (
 	"golang.org/x/net/html"
 )
 
-// type Link struct {
-// 	Href string
-// 	text string
-// }
+type Link struct {
+	Href string
+	Text string
+}
 
 func Parse(r io.Reader) {
 	node, err := html.Parse(r)
@@ -21,10 +21,28 @@ func Parse(r io.Reader) {
 	dfs(node)
 }
 
+func fetchHref(node *html.Node, link *Link) {
+	for _, value := range node.Attr {
+		if value.Key == "href" {
+			link.Href = value.Val
+		}
+	}
+	//fetchText(node)
+}
+
+func fetchText(node *html.Node, link *Link) {
+	link.Text = node.Data
+}
+
 func dfs(node *html.Node) {
 	if node.Type == html.ElementNode && node.Data == "a" {
-		fmt.Println(node)
-		return
+		var link Link
+		fetchHref(node, &link)
+		next := node.FirstChild
+		if next.Type == html.TextNode {
+			fetchText(next, &link)
+		}
+		fmt.Println(link)
 	}
 	for node := node.FirstChild; node != nil; node = node.NextSibling {
 		dfs(node)
